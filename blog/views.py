6e650639aaf5bfdf9c21fff8947523a3fd82595f
Miguel Angel Falcón Díaz <miguel_falcon_dev@hotmail.com>
2021-04-02 19:41:24 -0600

    Cambios en las vistas del sitio de la app

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from blog.models import Entry, Practice
 
 # Create your views here.
-    
+
+def index(request):
+    return HttpResponse("Este es la app del proyecto")
--- journal_practice/urls.py
+++ journal_practice/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import index
 from blog.api.views import (
     EntriesListApiView,
     EntryDetailApiView
 )
 
 urlpatterns = [
+    path('', index)
     path('admin/', admin.site.urls),
     path('api-auth/', include ('rest_framework.urls')),
     path('blog/api/', EntriesListApiView.as_view()),
