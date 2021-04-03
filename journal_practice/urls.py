fe088255b4a3921eda4e38a1a32c8bb2134e4c06
Miguel Angel Falcón Díaz <miguel_falcon_dev@hotmail.com>
2021-04-02 21:40:17 -0600

    Actualización de migrations y actualización de urls.py

--- journal_practice/urls.py
+++ journal_practice/urls.py
@@ -25,7 +25,7 @@ from blog.api.views import (
 )
 
 urlpatterns = [
-    path('', index)
+    path('', index),
     path('admin/', admin.site.urls),
     path('api-auth/', include ('rest_framework.urls')),
     path('blog/api/', EntriesListApiView.as_view()),
